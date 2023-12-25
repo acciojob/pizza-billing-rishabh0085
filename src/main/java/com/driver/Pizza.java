b60253223efbf09b4df2caeff66b9ac22960ba77
Rishabh Singh <52191143+rishabh0085@users.noreply.github.com>
2023-12-25 08:56:03 +0530

    Update Pizza.java

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,33 +2,50 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;
+    private int basePrice;
     private Boolean isVeg;
+    private int extraCheesePrice;
+    private int extraToppingsPrice;
+    private int paperBagPrice;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        this.basePrice = isVeg ? 300 : 400;
+        this.extraCheesePrice = 0;
+        this.extraToppingsPrice = 0;
+        this.paperBagPrice = 0;
+        this.bill = "";
     }
 
     public int getPrice(){
-        return this.price;
+        return this.basePrice + this.extraCheesePrice + this.extraToppingsPrice + this.paperBagPrice;
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if (extraCheesePrice == 0) {
+            extraCheesePrice = 80;
+            this.bill += "Extra Cheese Added: " + extraCheesePrice + "\n";
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if (extraToppingsPrice == 0) {
+            extraToppingsPrice = isVeg ? 70 : 120;
+            this.bill += "Extra Toppings Added: " + extraToppingsPrice + "\n";
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if (paperBagPrice == 0) {
+            paperBagPrice = 20;
+            this.bill += "Paperbag Added: " + paperBagPrice + "\n";
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        this.bill = "Base Price Of The Pizza: " + basePrice + "\n" + this.bill;
+        this.bill += "Total Price: " + getPrice() + "\n";
         return this.bill;
     }
 }
